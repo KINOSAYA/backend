169a637327355e20f448bffe3aae009cd0ca6bda
KingKord <93544537+KingKord@users.noreply.github.com>
2023-11-11 15:32:47 +0300

    rabbitmq emitter add

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -8,8 +8,10 @@ import (
 	"fmt"
 	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
+	"math"
 	"net/http"
 	"os"
+	"time"
 )
 
 // @title kinosaya API
@@ -24,7 +26,7 @@ var authHost = os.Getenv("authHost")
 var authGrpcPort = os.Getenv("authGrpcPort")
 
 func main() {
-	// TODO: try to connect to rabbitmq
+	// connecting to rabbitmq
 	rabbitConn, err := connect()
 	if err != nil {
 		log.Println(err)
@@ -50,6 +52,31 @@ func main() {
 }
 
 func connect() (*amqp.Connection, error) {
+	var counts int64
+	var backOff = 1 * time.Second
+	var connection *amqp.Connection
 
-	return nil, nil
+	// don't continue until rabbit is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		if err != nil {
+			fmt.Println("RabbitMQ not yet ready...")
+			counts++
+		} else {
+			log.Println("Connected to RabbitMQ!")
+			connection = c
+			break
+		}
+
+		if counts > 5 {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		backOff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(backOff)
+		continue
+	}
+	return connection, nil
 }
